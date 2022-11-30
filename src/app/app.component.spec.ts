37662df4c9803c621582506b2b969e9c57609228
Pietha Tiara <pipiethatiara@gmail.com>
2022-11-30 08:20:04 +0700

    for deployment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'ng-batch-four'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-batch-four');
+    expect(app).toEqual('ng-batch-four');
   });
 
   it('should render title', () => {
