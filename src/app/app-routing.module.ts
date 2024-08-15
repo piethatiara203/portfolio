70bc4ccf1abf71b921c18a77580f3d52583104cb
ap-pietha-tiara <124659780+ap-pietha-tiara@users.noreply.github.com>
2024-08-15 13:50:32 +0700

    Deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,46 +1,8 @@
-import { Component, NgModule } from '@angular/core';
+import  { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { PlaygroundComponent } from './pages/playground/playground.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { CalculateGasolineComponent } from './pages/calculate-gasoline/calculate-gasoline.component';
-import { UsersComponent } from './pages/users/users.component';
-import { AdminGuard } from './guards/admin.guard';
-import { UsersGuard } from './guards/users.guard';
 
 const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'playground',
-    component: PlaygroundComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
-  },
-  {
-    path: 'calculate',
-    component: CalculateGasolineComponent,
-  },
-  {
-    path: 'users',
-    component: UsersComponent,
-    canActivate: [UsersGuard],
-  },
-  {
-    path: 'admin',
-    loadChildren: () =>
-      import('./pages/admin/admin.module').then((m) => m.AdminModule),
-    canLoad: [AdminGuard],
-  },
-  {
-    path: '',
-    redirectTo: 'register',
-    pathMatch: 'full',
-  },
+
 ];
 
 @NgModule({
--- src/app/interfaces/response.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface ResponseBase<T> {
-  message: string;
-  data: T;
-}
